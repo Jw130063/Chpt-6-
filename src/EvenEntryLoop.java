e01c1157119e133113de48d790bcf9fdfeb22cb6
Joshua Warren <Jw130063@students.davenport.k12.ia.us>
2019-05-10 13:24:57 -0500

    update

--- src/EvenEntryLoop.java
+++ src/EvenEntryLoop.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class EvenEntryLoop {
-    
+    public static void main(String[] args){
+
+        int userEntry;
+        String result;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a even number.");
+        System.out.println("Enter a whole number>>>");
+        userEntry = input.nextInt();
+
+        if (userEntry%2 == 0) {
+            result = userEntry + "number is even";
+        }
+        else
+            result = userEntry + "number is odd";
+        System.out.println("Error");
+
+
+
+
+
+
+    }
+
 }
---
+++ src/TestScoreStatistics.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TestScoreStatistics {
+    public static void main(String [] args){
+       Scanner input= new Scanner(System.in);
+        int score;
+        int count=0;
+        int high=0;
+        int low=100;
+        int userEntry=0;
+        while (userEntry != 999) {
+            System.out.println("Please enter your test score");
+            userEntry = input.nextInt();
+            if(userEntry> high){
+                high= userEntry;
+            }
+            else if(userEntry<low){
+                low=userEntry;
+            }
+            count++;
+        }
+
+    }
+}
